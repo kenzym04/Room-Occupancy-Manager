797f06e7a0bff9f25928e9a4559268e121dd388a
Kennedy Alex Omondi Owiro <46674750+kenzym04@users.noreply.github.com>
2021-02-17 23:10:27 +0300

    Update RoomsController.java

--- src/main/java/com/smart_host/rooms/api/controller/RoomsController.java
+++ src/main/java/com/smart_host/rooms/api/controller/RoomsController.java
@@ -1,35 +1,22 @@
-/*
- * Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
- * with the License. A copy of the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
- * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
- */
 package com.smart_host.rooms.api.controller;
 
-
-import com.smart_host.rooms.api.model.Error;
-import com.smart_host.rooms.api.model.RoomOccupationOptimizationResponse;
-import com.smart_host.rooms.occupancy.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import com.smart_host.rooms.api.model.Error;
+import com.smart_host.rooms.api.model.RoomOccupationOptimizationResponse;
+import com.smart_host.rooms.occupancy.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
