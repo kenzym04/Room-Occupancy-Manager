7a088dadbf621eb615c59bac64bb5a667cb05229
Kennedy Alex Omondi Owiro <46674750+kenzym04@users.noreply.github.com>
2021-02-17 23:20:14 +0300

    Addition of spring application
    Addition of spring application

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ mvn package
 sam package --template-file sam.yaml --s3-bucket jonathanmv-smart-host-code-challenge --output-template-file target/deploy.yaml
 sam deploy --template-file target/deploy.yaml --stack-name smart-host-code-challenge --capabilities CAPABILITY_IAM
 
+
